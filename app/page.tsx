8411b71c0d6724d499980406206c4a91126f9ae3
Igor_Titov <mrfreiii@gmail.com>
2025-07-18 13:05:06 +0300

    added app layout and updated global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Container, Title } from "@/components/shared";
 
 export default function Home() {
     return (
         <>
-            <div>
-                HEllO WORLD
-            </div>
-
-            <Button>Корзина</Button>
+            <Container className={"mt-10"}>
+                <Title text={"Все пиццы"} size={"lg"} className={"font-extrabold"}/>
+            </Container>
         </>
     );
 }
